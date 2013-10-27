f87370c15aae6a95ea7ae09734bd6f8b4df0c298
Enrique <eac2192@columbia.edu>
2013-10-27 19:22:09 -0400

    more work on front end

--- web/js/life.js
+++ web/js/life.js
@@ -1,10 +1,12 @@
 var app = angular.module('Conways',[]);
  
-app.controller('LifeCtrl', ['$scope', '$http', function($scope, $http) {
+app.controller('LifeCtrl', ['$scope', '$http', '$q', function($scope, $http, $q) {
     $scope.map = Array(1);
+    $scope.map_size = Array(1);
     $scope.n_rows = 0;
     $scope.n_columns = 0;
     $scope.canonical = 0;
+    $scope.board = "";
     var ALIVE = 1;
     var DEAD = 0;
 
@@ -37,38 +39,58 @@ app.controller('LifeCtrl', ['$scope', '$http', function($scope, $http) {
 
     $scope.fromCanonical = function(canonical) {
       var t = canonical.split("|");
-      var size = t[0];
-      $scope.n_columns = size[0];
-      $scope.n_rows = size[1];
+      var size = t[0].split(",");
+      var n_columns = parseInt(size[0]);
+      var n_rows = parseInt(size[1]);
       var elems = t[1];
       elems = elems.split(',');
 
       // INITIALIZE AN EMPTY GRIDs
-      $scope.map = Array($scope.n_columns);
+      var map = Array($scope.n_columns);
       for (var col = 0; col < $scope.n_columns; col++) {
-        $scope.map[col] = new Array($scope.n_rows);
+        map[col] = new Array($scope.n_rows);
         for (var row = 0; row < $scope.n_rows; row++) {
-            $scope.map[col][row] = DEAD;
+            map[col][row] = DEAD;
+            var elem = document.getElementById(col + '-' + row); 
+            elem.setAttribute('class', 'dead');
         }
       } 
-
       //LOOP THROUGH THE COMPUTED ALIVE AND ADD THEM TO MAP
-      for (var i=0; i<elements.length; i++) {
-        $scope.map[i][i+1] = ALIVE;
+      for (var i=0; i<elems.length-1; i=i+2) {
+        var col = parseInt(elems[i]);
+        var row = parseInt(elems[i+1]);
+        map[col][row] = ALIVE;
+        var elem = document.getElementById(col + '-' + row); 
+        elem.setAttribute('class', 'alive');
       }
+      //$scope.n_columns = n_columns;
+      //$scope.n_rows = n_rows;
+      $scope.map = map;
+    }
+
+    $scope.loop = function() {
+      setInterval($scope.nextGen, 100);
     }
 
     $scope.nextGen = function() {
       var url = "/next?state=" + $scope.canonical;
-      $scope.canonical = $http.get(url)
-      console.log($scope.canonical);
-      $scope.fromCanonical($scope.canonical)
+      $http.get(url).then(function(response) {
+          $scope.canonical = response.data
+          $scope.fromCanonical($scope.canonical);
+          console.log('ryder is a bich');
+          //$scope.toBoard();
+        }, function(response) {
+          return $q.reject(response.data.error);
+      });
+
     }
 
     $scope.initMap = function(n_columns, n_rows) {
       var map = Array(n_columns);
+      var temp = Array(n_columns);
       for (var col = 0; col < n_columns; col++) {
         map[col] = new Array(n_rows);
+        temp[col] = new Array(n_rows);
         for (var row = 0; row < n_rows; row++) {
           t = Math.random();
           if( t > 0.5) {
@@ -82,7 +104,24 @@ app.controller('LifeCtrl', ['$scope', '$http', function($scope, $http) {
       $scope.n_rows = n_rows;
       $scope.n_columns = n_columns;
       $scope.map = map;
+      $scope.map_size = temp;
       $scope.canonical = $scope.toCanonical();
+      //$scope.toBoard();
+    }
+
+    $scope.toBoard = function() {
+      console.log('starting');
+      var map = '<ul class="columns">'
+      for (var col = 0; col < $scope.n_columns; col++) {
+        map += '<li class="col"><ul>'
+        for (var row = 0; row < $scope.n_rows; row++) {
+          elem = $scope.getElem(col, row);
+          map += '<li class="cell"><img src="/static/images/'+ elem + '"></img></li>';
+        } 
+        map += '</ul></li>';
+      } 
+      map += '</ul>';
+      $scope.board = map;
     }
 
 }]);
