4f2c01201c3ae80d11a216a850885c0e6aa2d34c
Enrique <eac2192@columbia.edu>
2013-10-29 19:26:37 -0400

    some more d3 work

--- web/js/life.js
+++ web/js/life.js
@@ -12,11 +12,13 @@ app.controller('LifeCtrl', ['$scope', '$http', '$q', function($scope, $http, $q)
     var ALIVE = 1;
     var DEAD = 0;
 
+    $scope.visited_colors = ['#EEE', '#CCC', '#AAA', '#888', '#666', '#444', '#222'];
+
 
     $scope.getCol = function(col) {
       return $scope.map[col];
     }
-    
+
     $scope.getElem = function(col, row) {
       if ($scope.map[col][row] == ALIVE)
         return 'cell.gif';
@@ -37,6 +39,21 @@ app.controller('LifeCtrl', ['$scope', '$http', '$q', function($scope, $http, $q)
       return canonical;
     }
 
+    $scope.color = function(col, row) {
+      var cell = $scope.map[col][row];
+      if (cell.alive == 1) {
+        return 'red';
+      }
+      else {
+        if (cell.n_dead < $scope.visited_colors.length) {
+          return $scope.visited_colors[cell.n_dead];
+        }
+        else {
+          return $scope.visited_colors[$scope.visited_colors.length-1];
+        }
+      }
+    }
+
     $scope.fromCanonical = function(canonical) {
       var t = canonical.split("|");
       var size = t[0].split(",");
@@ -45,23 +62,29 @@ app.controller('LifeCtrl', ['$scope', '$http', '$q', function($scope, $http, $q)
       var elems = t[1];
       $scope.alive = elems.split(',');
 
+      var cells = d3.select('.cell');
+      console.log(cells); 
       // KILL OF ALIVE CELLS FROM PREV
       for (var i=0; i< $scope.prev.length-1; i=i+2) {
         var col = parseInt($scope.prev[i]);
         var row = parseInt($scope.prev[i+1]);
-        d3.select('#C'+ col + 'R' + row).attr("class", "cell dead");
+        $scope.map[col][row].alive = 0;
+        $scope.map[col][row].n_dead += 1;
+        //d3.select('#C'+ col + 'R' + row).attr("class", "cell dead").attr("n_dead", function(d) {
+          //console.log(d.n_dead);
+          //return d.n_dead;
+        //}).style("fill", function(d) {return $scope.color(d.n_dead);});
       }
 
       //LOOP THROUGH THE COMPUTED ALIVE AND ADD THEM TO MAP
       for (var i=0; i<$scope.alive.length-1; i=i+2) {
         var col = parseInt($scope.alive[i]);
         var row = parseInt($scope.alive[i+1]);
-        console.log('#C'+ col + 'R' + row);
-        d3.select('#C'+ col + 'R' + row).attr("class", "cell alive");
+        $scope.map[col][row].alive = 1;
+        //d3.select('#C'+ col + 'R' + row).attr("class", "cell alive");
       }
 
       $scope.prev = $scope.alive;
-      $scope.map = map;
     }
 
     $scope.loop = function() {
@@ -76,11 +99,27 @@ app.controller('LifeCtrl', ['$scope', '$http', '$q', function($scope, $http, $q)
           $scope.canonical = response.data
           console.log('from canonical');
           $scope.fromCanonical($scope.canonical);
+          $scope.updateGrid('chart')
           //$scope.toBoard();
         }, function(response) {
           return $q.reject(response.data.error);
       });
 
+    }
+
+    function CellGrid(rows, columns) {
+      this.cells = new Array(rows);
+      var n = 0;
+      for(var i = -1; ++i < rows;){
+          this.cells[i] = new Array(columns);
+          for(var j = -1; ++j < columns; ){
+              var cell = new Cell(false);
+              cell.n = n++;
+              cell.x = i;
+              cell.y = j;
+              this.cells[i][j] = cell;
+          }
+      }
     }
 
     $scope.initMap = function(n_columns, n_rows) {
@@ -106,31 +145,39 @@ app.controller('LifeCtrl', ['$scope', '$http', '$q', function($scope, $http, $q)
       $scope.canonical = $scope.toCanonical();
     }
 
+    $scope.updateGrid = function(id) {
+      var grid = d3.select(id).append("svg")
+                      .attr("width", $scope.width)
+                      .attr("height", $scope.width)
+                      .attr("class", "chart");
+
+      var row = grid.selectAll(".row")
+                    .data($scope.map)
+                  .enter().append("svg:g")
+                    .attr("class", "row");
+
+      var col = row.selectAll(".cell")
+                   .data(function (d) { return d; })
+                  .enter().append("svg:rect")
+                   .attr("class", "cell new")
+                   .attr("x", function(d) { return d.x; })
+                   .attr("y", function(d) { return d.y; })
+                   .attr("width", function(d) { return d.width; })
+                   .attr("height", function(d) { return d.height; })
+                   .attr("id", function(d) {return d.index; })
+                   .attr("n_dead", function(d) { return d.n_dead; })
+                   .style("fill", function(d) {return $scope.color(d.col, d.row); })
+    }
+
+
     $scope.generateGrid = function (id, width, height, n_columns, n_rows)
     {
         $scope.map = $scope.randomData(width, height, n_columns, n_rows);
         $scope.n_columns = n_columns; 
         $scope.n_rows = n_rows;
-        //console.log($scope.map);
-        var grid = d3.select(id).append("svg")
-                        .attr("width", width)
-                        .attr("height", height)
-                        .attr("class", "chart");
-
-        var row = grid.selectAll(".row")
-                      .data($scope.map)
-                    .enter().append("svg:g")
-                      .attr("class", "row");
-
-        var col = row.selectAll(".cell")
-                     .data(function (d) { return d; })
-                    .enter().append("svg:rect")
-                     .attr("class", "cell new")
-                     .attr("x", function(d) { return d.x; })
-                     .attr("y", function(d) { return d.y; })
-                     .attr("width", function(d) { return d.width; })
-                     .attr("height", function(d) { return d.height; })
-                     .attr("id", function(d) {return d.index; })
+        $scope.width = width;
+        $scope.height = height;
+        $scope.updateGrid(id);
         $scope.canonical = $scope.toCanonical();
     }
 
@@ -149,10 +196,10 @@ app.controller('LifeCtrl', ['$scope', '$http', '$q', function($scope, $http, $q)
         var count = 0;
         var status = 0;
 
-        for (var index_a = 0; index_a < n_rows; index_a++)
+        for (var index_a = 0; index_a < n_columns; index_a++)
         {
             $scope.map.push(new Array());
-            for (var index_b = 0; index_b < n_columns; index_b++)
+            for (var index_b = 0; index_b < n_rows; index_b++)
             {
                 newValue = Math.random();
                 if (newValue > 0.5) {
@@ -162,13 +209,16 @@ app.controller('LifeCtrl', ['$scope', '$http', '$q', function($scope, $http, $q)
                   status = DEAD;
                 }
                  $scope.map[index_a].push({ 
+                                    col: index_a,
+                                    row: index_b,
                                     alive: status,
                                     width: gridItemWidth,
                                     height: gridItemHeight,
                                     x: xpos,
                                     y: ypos,
                                     count: count,
-                                    index: 'C' + index_a + 'R' + index_b
+                                    index: 'C' + index_a + 'R' + index_b,
+                                    n_dead: 0
                                 });
                 
                 xpos += stepX;
